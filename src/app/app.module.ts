f7baafeb9a490c84f7e17650ae3ffea1b4d01e1e
Dioonel <dionelmartinezc@gmail.com>
2023-04-08 12:24:26 -0300

    Completed nav component
    Initialized every component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContentComponent } from './components/content/content.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { FormComponent } from './components/form/form.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PlansComponent } from './components/plans/plans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ContentComponent,
+    ReviewsComponent,
+    FormComponent,
+    FooterComponent,
+    PlansComponent
   ],
   imports: [
     BrowserModule
